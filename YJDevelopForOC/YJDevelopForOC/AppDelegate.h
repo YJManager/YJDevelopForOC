1047500a558e2cd853929116f536b62714147975
yuejun.hou <8069060@qq.com>
2016-12-26 14:48:19 +0800

    662

---
+++ YJDevelopForOC/YJDevelopForOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJDevelopForOC
+//
+//  Created by YJHou on 2016/12/26.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
